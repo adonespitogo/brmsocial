376fcadd47d188ecdf3f576ca63f95e9f79a2b91
Adones <pitogo.adones@gmail.com>
2014-02-12 08:06:32 +0800

    ng bind for user fullname

--- app/views/home/vendor/header.blade.php
+++ app/views/home/vendor/header.blade.php
@@ -25,7 +25,7 @@
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown"> @{{currentUser.firstname}} @{{currentUser.lastname}} <span class="glyphicon glyphicon-cog"></span></a>
+              <a class="dropdown-toggle" data-toggle="dropdown"> <span ng-bind="currentUser.fullname"></span>  <span class="glyphicon glyphicon-cog"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a ng-click="copyCurrentUser()" href="#account-settings" data-toggle="modal">My Account</a></li>
                 <li><a href="{{URL::to('session/logout')}}">Logout</a></li>
