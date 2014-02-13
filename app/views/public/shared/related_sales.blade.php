7f1f837280081358691340cc9b22329d0ee4ee1f
Adones <pitogo.adones@gmail.com>
2014-02-13 13:40:29 +0800

    Related products picture

--- app/views/public/shared/related_sales.blade.php
+++ app/views/public/shared/related_sales.blade.php
@@ -3,7 +3,7 @@
 	<ul>
 		@foreach(Product::all() as $product)
 			<li>
-				<img src='{{URL::to("website/images/products/small/img-cloudseekr.jpg")}}' alt="related deals" title="related deals">
+				<img src='{{URL::to($product->pictures[0]->picture->url("small"))}}' alt="related deals" title="related deals">
 				<div><a href="#">{{$product->product_name}}</a></div>
 				<div class="price">${{$product->discounted_price}}</div>
 				<ul>
