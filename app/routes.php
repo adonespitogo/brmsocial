93ff16cb89953270626684ef10f1fd7b5fb4d1ba
Adones <pitogo.adones@gmail.com>
2014-02-20 14:13:35 +0800

    fix routes

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::get('/', array('uses' => 'PublicController@index'));
 
 
 // start public routes
+Route::get('/', array('uses' => 'PublicController@index'));
+Route::get('product/{slug}', array('uses' => 'PublicController@product'));
 Route::controller('session', 'SessionController');
 
 
@@ -19,7 +20,6 @@ Route::group(array('before' => 'auth'), function(){
 	
 	Route::controller('home', 'HomeController');
 
-	Route::get('product/{slug}', array('uses' => 'PublicController@product'));
 	Route::get('products/my-products', array('uses' => 'ProductController@myProducts'));
 	Route::get('products/my-active-products', array('uses' => 'ProductController@myActiveProducts'));
 	Route::get('products/my-active-products-count', array('uses' => 'ProductController@myActiveProductsCount'));
