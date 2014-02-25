6ff90040e8ef137e428b6505929142bc85baf488
Adones <pitogo.adones@gmail.com>
2014-02-25 15:33:19 +0800

    admin header padding

--- app/views/home/admin/header.blade.php
+++ app/views/home/admin/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="">
+      <div style="padding-left: 15px; padding-right: 15px;">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
