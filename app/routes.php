1c064ecf13eed85b5746bf7989bff5ec6f6e12a4
Adones <pitogo.adones@gmail.com>
2014-02-26 16:06:34 +0800

    Register with twitter

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,10 @@ Route::get('/', array('uses' => 'PublicController@index'));
 Route::get('product/{slug}', array('uses' => 'PublicController@product'));
 Route::get('category/{slug}', array('uses' => 'CategoryController@getProducts'));
 Route::controller('session', 'SessionController');
-Route::controller('register', 'RegisterController');
+Route::get('register', array('uses' => 'RegisterController@index'));
+Route::get('register/facebook', array('uses' => 'RegisterController@registerViaFacebook'));
+Route::get('register/twitter', array('uses' => 'RegisterController@registerViaTwitter'));
+Route::post('register', array('uses' => 'RegisterController@create'));
 Route::controller('cart', 'CartController');
 Route::post('subscribe', array('uses' => 'PublicController@addSubscriber'));
 
