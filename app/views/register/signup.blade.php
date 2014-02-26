79c18cadc316ac2a7163cdf7f5f8ce98bd3f3d59
Adones <pitogo.adones@gmail.com>
2014-02-26 17:56:11 +0800

    signup view udpated

--- app/views/register/signup.blade.php
+++ app/views/register/signup.blade.php
@@ -12,7 +12,7 @@
 @section('body')
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-lg-4 col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2 col-lg-offset-4" style="padding-top: 50px;">
+			<div class="col-xs-12 col-lg-6 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-lg-offset-3" style="padding-top: 50px;">
 
 				<p class="text-center">
 					<img src="http://localhost/htdocs/repo.brmsocial.com/public/assets/brmsocial-icon.png">
@@ -47,10 +47,20 @@
 						
 					</div>
 					<div class="form-group">
-						<a href="{{URL::to('signup/facebook')}}" class="btn btn-info btn-block">
-							<i class="fa fa-facebook-square"></i>
-							Login with Facebook
-						</a>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="{{URL::to('signup/facebook')}}" class="btn btn-info btn-block">
+									<i class="fa fa-facebook-square"></i>
+									Login with Facebook
+								</a>
+							</div>
+							<div class="col-md-6">
+								<a href="{{URL::to('signup/twitter')}}" class="btn btn-info btn-block">
+									<i class="fa fa-twitter-square"></i>
+									Login with Twitter
+								</a>
+							</div>
+						</div>
 					</div>
 				</form>
 				
